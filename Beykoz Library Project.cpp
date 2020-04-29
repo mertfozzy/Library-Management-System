0f10019e285af334a68faec267172ff36e51be2a
Mert Altuntaş <59746147+mertfozzy@users.noreply.github.com>
2020-04-29 12:39:17 +0300

    Update Beykoz Library Project.cpp

--- Beykoz Library Project.cpp
+++ Beykoz Library Project.cpp
@@ -1,3 +1,14 @@
+/*This project is an C++ program that can manage a library. It can keep book datas, and make their followings.
+
+This program can use with just DEV-CPP v5.11. Other IDE's may not work.
+
+If you want to create your own book data, just copy the code and execute in a new folder. Then you can add new books
+in the program's menu. (Just use the program as usual.)
+
+Created by ; Mert Altuntas, Kaan Celiktas, Mahmutcan Aslan.*/
+
+
+
 #include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
